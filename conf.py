cbe3ad0b65242744f3c0c6dba3a3b377ab47a828
Finn <finn@finn.io>
2016-02-12 12:01:39 -0800

    Cleaned up conf a bit

--- conf.py
+++ conf.py
@@ -119,7 +119,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Hyperboria Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -185,7 +185,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'HyperboriaDocumentationdoc'
+htmlhelp_basename = 'HyperboriaDocumentation'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -205,7 +205,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'HyperboriaDocumentation.tex', u'Hyperboria Documentation Documentation',
+  ('index', 'HyperboriaDocumentation.tex', u'Hyperboria Documentation',
    u'The Hyperborians', 'manual'),
 ]
 
@@ -235,7 +235,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'hyperboriadocumentation', u'Hyperboria Documentation Documentation',
+    ('index', 'hyperboriadocumentation', u'Hyperboria Documentation',
      [u'The Hyperborians'], 1)
 ]
 
@@ -249,7 +249,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'HyperboriaDocumentation', u'Hyperboria Documentation Documentation',
+  ('index', 'HyperboriaDocumentation', u'Hyperboria Documentation',
    u'The Hyperborians', 'HyperboriaDocumentation', 'One line description of project.',
    'Miscellaneous'),
 ]
