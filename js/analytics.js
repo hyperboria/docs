a5dae85bea65e2c8cc57ecc94ce2148d07cc892e
Finn <finn@finn.io>
2016-06-09 11:55:54 -0700

    Update analytics site id

--- js/analytics.js
+++ js/analytics.js
@@ -4,7 +4,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
   var u="//analytics.seattlemesh.net/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', 20]);
+  _paq.push(['setSiteId', 10]);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
